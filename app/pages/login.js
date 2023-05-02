8ebf5fd2ab3a88cb7a65f9fc958f5561458a0262
Mac12880 <marc.juan6.mj@gmail.com>
2023-05-02 22:12:25 +0800

    fix header buttons and functions

--- app/pages/login.js
+++ app/pages/login.js
@@ -2,25 +2,41 @@ import LoginNavbar from '@/components/loginNavbar'
 import SLoginNavbar from '@/components/sLoginNavbar'
 import React, { useState } from 'react'
 
-import { RxHamburgerMenu } from "react-icons/rx"
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx"
 import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi"
 
 const Login = () => {
-  const [smallNavbar, setSmallNavbar] = useState(true)
+  const [smallNavbar, setSmallNavbar] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(true)
 
   const handleSmallNavbar = () => {
     setSmallNavbar((currentState) => !currentState)
     console.log("clicked")
   }
 
+  const handleShowNavbar = () => {
+    setShowNavbar((currentState) => !currentState)
+  }
+
+
+  const Navbar = () => {
+    if(smallNavbar === true){
+      return <SLoginNavbar />
+    }
+    else{
+      return <LoginNavbar />
+    }
+  }
+
   return (
     <div className='d-flex flex-direction-row justify-content-start'>
-      {/* <Navbar /> */}
-      {smallNavbar ? (<SLoginNavbar />) : (<LoginNavbar />)}
+      {/* {smallNavbar ? <SLoginNavbar /> : <LoginNavbar />} */}
+      {showNavbar && <Navbar/>}
       <div className='w-100 dashboard'>
-        <div className='d-flex w-100 headerDashboard text-white'>
-          <RxHamburgerMenu className='sloginNavbarIcon'/>
-          <BiArrowToLeft className='sloginNavbarIcon'/>
+        <div className='d-flex justify-content-between w-100 headerDashboard text-white'>
+          {showNavbar ? <RxCross1 className='headerDashboardIcon' onClick={handleShowNavbar}/> : <RxHamburgerMenu className='headerDashboardIcon' onClick={handleShowNavbar}/>}
+          {smallNavbar ? <BiArrowToRight className='headerDashboardIcon' onClick={handleSmallNavbar}/> : <BiArrowToLeft className='headerDashboardIcon' onClick={handleSmallNavbar}/>}
+          
         </div>
         <h1 className='m-2 p-3'>Content</h1>
       </div>
