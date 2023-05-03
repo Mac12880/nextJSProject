f4b85851b8d289a7e721e0eb4d1f85335a092ad7
Mac12880 <marc.juan6.mj@gmail.com>
2023-05-03 21:58:43 +0800

    change position of user and navbar size

--- app/components/loginNavbar.js
+++ app/components/loginNavbar.js
@@ -5,97 +5,157 @@ import { IoDocumentsOutline, IoNewspaperOutline } from "react-icons/io5"
 import { FiBook } from "react-icons/fi"
 import { IoMdBook } from "react-icons/io"
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2"
+import { Tooltip} from '@mui/material'
+// import DeleteIcon from 'mui/icons-material/Delete'
+import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi"
+
+const Navbar = () => {
+    const [smallNavbar, setSmallNavbar] = useState(true)
+
+    const handleSmallNavbar = () => {
+    setSmallNavbar((currentState) => !currentState)
+    console.log("clicked")
+    }
+
+    const SLoginNavbar = () => {
+      return (
+          <div className='sloginNavbar navbarShadow d-flex flex-column flex-shrink-0'>
+              <Tooltip title="TAGPROS" placement="right" arrow>
+                  <a href='/' className='d-flex justify-content-center py-3 px-0 link-body-emphasis text-decoration-none'
+                  data-bs-toggle='tooltip' data-bs-placement='right' data-bs-original-title='TAGPROS'>
+                      <img className="loginNavbarLogo" src="/tagproslogo.png" alt="TAGPROS" />
+                  </a>
+              </Tooltip>
+              <hr className='m-0' />
+              <ul className='nav nav-pills flex-column mb-auto'>
+                  <li className='nav-item'>
+                      <Tooltip title="Home" placement="right" arrow>
+                      <a href='#' className='nav-link border-bottom py-2 px-0 rounded-0 d-flex justify-content-center'>
+                          <BsHouse className='sloginNavbarIcon bi pe-none text-black' />
+                      </a>
+                      </Tooltip>
+                  </li>
+                  <li className='nav-item'>
+                      <Tooltip title="Dashboard" placement="right" arrow>
+                      <a href='#' className='nav-link border-bottom py-2 px-0 rounded-0 d-flex justify-content-center'>
+                          <AiOutlineDashboard className='sloginNavbarIcon bi pe-none text-black' />
+                      </a>
+                      </Tooltip>
+                  </li>
+                  <li className='nav-item'>
+                      <Tooltip title="Documents" placement="right" arrow>
+                      <a href='#' className='nav-link border-bottom py-2 px-0 rounded-0 d-flex justify-content-center'>
+                          <IoDocumentsOutline className='sloginNavbarIcon bi pe-none text-black' />
+                      </a>
+                      </Tooltip>
+                  </li>
+                  <li className='nav-item'>
+                      <Tooltip title="Classes" placement="right" arrow>
+                      <a href='#' className='nav-link border-bottom py-2 px-0 rounded-0 d-flex justify-content-center'>
+                          <FiBook className='sloginNavbarIcon bi pe-none text-black' />
+                      </a>
+                      </Tooltip>
+                  </li>
+                  <li className='nav-item'>
+                      <Tooltip title="My Classes" placement="right" arrow>
+                      <a href='#' className='nav-link border-bottom py-2 px-0 rounded-0 d-flex justify-content-center'>
+                          <IoMdBook className='sloginNavbarIcon bi pe-none text-black' />
+                      </a>
+                      </Tooltip>
+                  </li>
+                  <li className='nav-item'>
+                      <Tooltip title="Jobs" placement="right" arrow>
+                      <a href='#' className='nav-link border-bottom py-2 px-0 rounded-0 d-flex justify-content-center'>
+                          <HiOutlineMagnifyingGlass className='sloginNavbarIcon bi pe-none text-black' />
+                      </a>
+                      </Tooltip>
+                  </li>
+                  <li className='nav-item'>
+                      <Tooltip title="Job Applications" placement="right" arrow>
+                      <a href='#' className='nav-link border-bottom py-2 px-0 rounded-0 d-flex justify-content-center'>
+                          <IoNewspaperOutline className='sloginNavbarIcon bi pe-none text-black' />
+                      </a>
+                      </Tooltip>
+                  </li>
+              </ul>
+              <hr className='m-0'/>
+              <div className='d-flex justify-content-center m-3 mb-4'>
+                  <BiArrowToRight className='headerDashboardIcon' onClick={handleSmallNavbar}/>
+              </div>
+              
+          </div>
+      )
+      }
+
+
+      const LoginNavbar = () => {
+        return (
+            <div className='loginNavbar navbarShadow d-flex flex-column flex-shrink-0 p-3'>
+              <a href='/' className='d-flex justify-content-center  link-body-emphasis text-decoration-none'>
+                <img className="loginNavbarLogo" src="/tagproslogo.png" alt="TAGPROS" />
+                {/* <span className='loginNavbarLogoText text-black'>TAGPROS</span> */}
+              </a>
+              <hr />
+              <ul className='nav nav-pills flex-column mb-auto'>
+                <li className='nav-item'>
+                  <a href='#' className='nav-link link-body-emphasis text-black'>
+                    <BsHouse className='loginNavbarIcon bi pe-none me-2 ' />
+                    Home
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#' className='nav-link link-body-emphasis text-black'>
+                    <AiOutlineDashboard className='loginNavbarIcon bi pe-none me-2' />
+                    Dashboard
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#' className='nav-link link-body-emphasis text-black'>
+                    <IoDocumentsOutline className='loginNavbarIcon bi pe-none me-2' />
+                    Documents
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#' className='nav-link link-body-emphasis text-black'>
+                    <FiBook className='loginNavbarIcon bi pe-none me-2' />
+                    Classes
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#' className='nav-link link-body-emphasis text-black'>
+                    <IoMdBook className='loginNavbarIcon bi pe-none me-2' />
+                    My Classes
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#' className='nav-link link-body-emphasis text-black'>
+                    <HiOutlineMagnifyingGlass className='loginNavbarIcon bi pe-none me-2' />
+                    Jobs
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#' className='nav-link link-body-emphasis text-black'>
+                    <IoNewspaperOutline className='loginNavbarIcon bi pe-none me-2' />
+                    Job Applications
+                  </a>
+                </li>
+              </ul>
+              <hr />
+              <div className='d-flex justify-content-center'>
+              <BiArrowToLeft className='headerDashboardIcon m-1 ' onClick={handleSmallNavbar}/>
+              </div>
+            </div>
+        )
+      }
 
 
 
-const LoginNavbar = () => {
-  const [toggler, setToggler] = useState(false)
-
-  const handleToggle = () => {
-    setToggler((currentState) => !currentState)
-    // console.log("clicked")
-  }
-  return (
-      <div className='loginNavbar navbarShadow d-flex flex-column flex-shrink-0 p-3'>
-        <a href='/' className='d-flex justify-content-center  link-body-emphasis text-decoration-none'>
-          <img className="loginNavbarLogo" src="/tagproslogo.png" alt="TAGPROS" />
-          {/* <span className='loginNavbarLogoText text-black'>TAGPROS</span> */}
-        </a>
-        <hr />
-        <ul className='nav nav-pills flex-column mb-auto'>
-          <li className='nav-item'>
-            <a href='#' className='nav-link link-body-emphasis text-black'>
-              <BsHouse className='loginNavbarIcon bi pe-none me-2 ' />
-              Home
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#' className='nav-link link-body-emphasis text-black'>
-              <AiOutlineDashboard className='loginNavbarIcon bi pe-none me-2' />
-              Dashboard
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#' className='nav-link link-body-emphasis text-black'>
-              <IoDocumentsOutline className='loginNavbarIcon bi pe-none me-2' />
-              Documents
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#' className='nav-link link-body-emphasis text-black'>
-              <FiBook className='loginNavbarIcon bi pe-none me-2' />
-              Classes
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#' className='nav-link link-body-emphasis text-black'>
-              <IoMdBook className='loginNavbarIcon bi pe-none me-2' />
-              My Classes
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#' className='nav-link link-body-emphasis text-black'>
-              <HiOutlineMagnifyingGlass className='loginNavbarIcon bi pe-none me-2' />
-              Jobs
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#' className='nav-link link-body-emphasis text-black'>
-              <IoNewspaperOutline className='loginNavbarIcon bi pe-none me-2' />
-              Job Applications
-            </a>
-          </li>
-        </ul>
-        <hr />
-        <div className='dropdown'>
-          <button 
-          className='d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle text-black border border-light bg-white'
-          onClick={handleToggle}
-          >
-            <img src="/profile.jpg"
-            className='rounded-circle me-2 loginProfilePic'>
-            </img>
-            <strong>Marc</strong>
-          </button>
-          <ul className='dropdown-menu text-small shadow show navbarProfileDD'
-          style={{
-            display: toggler === false && 'none'
-          }}
-          data-popper-placement='top-start'>
-            <li>
-              <a className='dropdown-item' href='#'>Profile</a>
-            </li>
-            <li>
-              <hr className='dropdown-divider' />
-            </li>
-            <li>
-              <a className='dropdown-item' href='#'>Sign Out</a>
-            </li>
-          </ul>
-        </div>
-        <div className='b-example-divider b-example-vr'></div>
-      </div>
-  )
+    return (
+        // {smallNavbar ? <SLoginNavbar /> : <LoginNavbar />}
+        <>
+            {smallNavbar ? <SLoginNavbar /> : <LoginNavbar />}
+        </>
+    )
 }
 
-export default LoginNavbar
+export default Navbar
--- app/pages/login.js
+++ app/pages/login.js
@@ -1,42 +1,54 @@
-import LoginNavbar from '@/components/loginNavbar'
-import SLoginNavbar from '@/components/sLoginNavbar'
 import React, { useState } from 'react'
-
+import LoginNavbar from '@/components/loginNavbar'
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx"
-import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi"
 
 const Login = () => {
-  const [smallNavbar, setSmallNavbar] = useState(false)
-  const [showNavbar, setShowNavbar] = useState(true)
+  const [toggler, setToggler] = useState(false)
 
-  const handleSmallNavbar = () => {
-    setSmallNavbar((currentState) => !currentState)
-    console.log("clicked")
+  const handleToggle = () => {
+  setToggler((currentState) => !currentState)
+  // console.log("clicked")
   }
 
+  const [showNavbar, setShowNavbar] = useState(true)
   const handleShowNavbar = () => {
     setShowNavbar((currentState) => !currentState)
-  }
-
-
-  const Navbar = () => {
-    if(smallNavbar === true){
-      return <SLoginNavbar />
-    }
-    else{
-      return <LoginNavbar />
-    }
   }
 
   return (
     <div className='d-flex flex-direction-row justify-content-start'>
       {/* {smallNavbar ? <SLoginNavbar /> : <LoginNavbar />} */}
-      {showNavbar && <Navbar/>}
+      {showNavbar && <LoginNavbar/>}
       <div className='w-100 dashboard'>
         <div className='d-flex justify-content-between w-100 headerDashboard text-white align-items-center '>
           {showNavbar ? <RxCross1 className='headerDashboardIcon' onClick={handleShowNavbar}/> : <RxHamburgerMenu className='headerDashboardIcon' onClick={handleShowNavbar}/>}
           <b className='loginNavbarLogoText'>TAGPROS</b>
-          {smallNavbar ? <BiArrowToRight className='headerDashboardIcon' onClick={handleSmallNavbar}/> : <BiArrowToLeft className='headerDashboardIcon' onClick={handleSmallNavbar}/>}
+          <div className='dropdown cursorPointer '>
+                <a
+                className='d-flex align-items-center justify-content-center text-decoration-none dropdown-toggle bg-transparent text-white'
+                onClick={handleToggle}
+                >
+                <img src="/profile.jpg"
+                className='rounded-circle  loginProfilePic'>
+                </img>
+                {/* <strong>Marc</strong> */}
+                </a>
+                <ul className='dropdown-menu text-small shadow show navbarProfileDD'
+                style={{
+                display: toggler === false && 'none'
+                }}
+                data-popper-placement='top-start'>
+                <li>
+                    <a className='dropdown-item' href='#'>Profile</a>
+                </li>
+                <li>
+                    <hr className='dropdown-divider' />
+                </li>
+                <li>
+                    <a className='dropdown-item' href='#'>Sign Out</a>
+                </li>
+                </ul>
+            </div>
           
         </div>
         <h1 className='m-2 p-3'>Content</h1>
